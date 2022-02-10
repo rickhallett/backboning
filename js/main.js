2968300f3f100adb4255aef143816a05ed97e151
Rick Hallett <rick.hallett@brandwatch.com>
2022-02-10 19:40:27 +0000

    collections challenge complete

--- js/main.js
+++ js/main.js
@@ -14,10 +14,7 @@ var Vehicle = Backbone.Model.extend({
     },
 });
 
-var bim = new Vehicle({ registrationNumber: 'XLI887' });
-console.log(bim);
-console.log('valid:', bim.isValid());
-bim.start();
+var firstCar = new Vehicle({ registrationNumber: 'XLI887' });
 
 var Car = Vehicle.extend({
     start: function () {
@@ -25,8 +22,33 @@ var Car = Vehicle.extend({
     },
 });
 
-var brum = new Car({ registrationNumber: 'XLI887' });
+var bimmer = new Car({ registrationNumber: 'XLI887' });
 
-console.log(brum);
-console.log('valid:', brum.isValid(), brum.validationError);
-brum.start();
+var Vehicles = Backbone.Collection.extend({
+    model: Vehicle,
+    url: 'api/vehicles/collection',
+});
+
+var fleet = new Vehicles([
+    new Vehicle({ registrationNumber: 'XLI887', colour: 'white' }),
+    new Vehicle({ registrationNumber: 'ZNP123', colour: 'blue' }),
+    new Vehicle({ registrationNumber: 'XUV456', colour: 'blue' }),
+]);
+
+// Find all the Blue cars and log them in the console.
+var blueCars = fleet.where({ colour: 'blue' });
+console.log(blueCars);
+
+// Find the car with the registration number XLI887 and log it in the console.
+var wanted = fleet.findWhere({ registrationNumber: 'XLI887' });
+console.log(wanted);
+
+// Remove this car from the collection.
+fleet.remove(wanted);
+console.log(fleet.length);
+
+// Convert the collection to a JSON object and log it in the console.
+console.log(fleet.toJSON());
+
+// Iterate the collection and log each car in the console.
+fleet.each((car) => console.log(car));
