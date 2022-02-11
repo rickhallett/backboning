91c003a81b1a1d207d36ee50154f6ab4a3163e9e
Rick Hallett <rick.hallett@brandwatch.com>
2022-02-11 19:32:09 +0000

    prettierfy bus.js

--- js/bus.js
+++ js/bus.js
@@ -1,90 +1,83 @@
-
 var Venue = Backbone.Model.extend();
 
 var Venues = Backbone.Collection.extend({
-	model: Venue
+    model: Venue,
 });
 
 var VenueView = Backbone.View.extend({
-	tagName: "li",
+    tagName: 'li',
 
-	initialize: function(options){
-		this.bus = options.bus;
-	},
+    initialize: function (options) {
+        this.bus = options.bus;
+    },
 
-	events: {
-		"click": "onClick",
-	},
+    events: {
+        click: 'onClick',
+    },
 
-	onClick: function(){
-		this.bus.trigger("venueSelected", this.model);
-	},
+    onClick: function () {
+        this.bus.trigger('venueSelected', this.model);
+    },
 
-	render: function(){
-		this.$el.html(this.model.get("name"));
+    render: function () {
+        this.$el.html(this.model.get('name'));
 
-		return this;
-	}
+        return this;
+    },
 });
 
 var VenuesView = Backbone.View.extend({
-	tagName: "ul",
+    tagName: 'ul',
 
-	id: "venues",
+    id: 'venues',
 
-	initialize: function(options){
-		this.bus = options.bus;
-	},
+    initialize: function (options) {
+        this.bus = options.bus;
+    },
 
-	render: function(){
-		var self = this;
+    render: function () {
+        var self = this;
 
-		this.model.each(function(venue){
-			var view = new VenueView({ model: venue, bus: self.bus });
-			self.$el.append(view.render().$el);
-		});
+        this.model.each(function (venue) {
+            var view = new VenueView({ model: venue, bus: self.bus });
+            self.$el.append(view.render().$el);
+        });
 
-		return this;
-	}
+        return this;
+    },
 });
 
 var MapView = Backbone.View.extend({
-	el: "#map-container",
+    el: '#map-container',
 
-	initialize: function(options){
-		this.bus = options.bus;
+    initialize: function (options) {
+        this.bus = options.bus;
 
-		this.bus.on("venueSelected", this.onVenueSelected, this);
-	},
+        this.bus.on('venueSelected', this.onVenueSelected, this);
+    },
 
-	onVenueSelected: function(venue){
-		this.model = venue;
-		this.render();
-	},
+    onVenueSelected: function (venue) {
+        this.model = venue;
+        this.render();
+    },
 
-	render: function(){
-		if (this.model)
-			this.$("#venue-name").html(this.model.get("name"));
+    render: function () {
+        if (this.model) this.$('#venue-name').html(this.model.get('name'));
 
-		return this;
-	}
-})
+        return this;
+    },
+});
 
 var bus = _.extend({}, Backbone.Events);
 
 var venues = new Venues([
-	new Venue({ name: "30 Mill Espresso" }),
-	new Venue({ name: "Platform Espresso" }),
-	new Venue({ name: "Mr Foxx" })
-	]);
+    new Venue({ name: '30 Mill Espresso' }),
+    new Venue({ name: 'Platform Espresso' }),
+    new Venue({ name: 'Mr Foxx' }),
+]);
 
 var venuesView = new VenuesView({ model: venues, bus: bus });
-$("#venues-container").html(venuesView.render().$el);
+$('#venues-container').html(venuesView.render().$el);
 
 var mapView = new MapView({ bus: bus });
 mapView.render();
-
-
-
-
-
