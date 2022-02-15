dead4744eb035c219471a365d0ef04cabdf64406
Rick Hallett <rick.hallett@brandwatch.com>
2022-02-15 14:54:50 +0000

    creating models from fetch results

--- omdb/js/src/collections.js
+++ omdb/js/src/collections.js
@@ -1,5 +1,19 @@
-define(['underscore', 'backbone'], function (_, Backbone) {
-    let Films = Backbone.Collection.extend({});
+define(['underscore', 'backbone', 'src/models'], function (
+    _,
+    Backbone,
+    models
+) {
+    let Films = Backbone.Collection.extend({
+        model: function (attrs, options) {
+            // console.log(attrs, options);
+            // debugger;
+            if (attrs.type !== 'id') {
+                throw new Error('unknown model type:', attrs.type);
+            }
+
+            return new models.Film(attrs, options);
+        },
+    });
 
     return {
         Films,
--- omdb/js/src/models.js
+++ omdb/js/src/models.js
@@ -1,14 +1,47 @@
-define(['underscore', 'backbone'], function (_, Backbone) {
+define(['underscore', 'backbone', 'src/collections'], function (
+    _,
+    Backbone,
+    collections
+) {
     let Film = Backbone.Model.extend({
+        defaults: {
+            type: 'id',
+        },
+
         initialize: function (options) {
+            console.log(options);
             this.url = `https://www.omdbapi.com/?i=${options.id}&apikey=6d7ddf73`;
+        },
+    });
+
+    let FilmSearch = Backbone.Model.extend({
+        defaults: _.extend({}, Film.prototype.defaults, {
+            type: 'title',
+        }),
+
+        initialize: function (options) {
+            this.url = `https://www.omdbapi.com/?s=${options.title}&apikey=6d7ddf73`;
         },
     });
 
     const lotr = new Film({ id: 'tt0120737' });
     lotr.fetch();
 
-    console.log(lotr);
+    const theBatmans = new collections.Films();
+    const searchResults = new FilmSearch({ title: 'dark knight' });
+    searchResults.fetch({
+        success: function () {
+            searchResults.get('Search').forEach((result) => {
+                const newFilm = new Film({ id: result.imdbID });
+                newFilm.fetch({
+                    success: function (data) {
+                        console.log(data);
+                        theBatmans.add(data);
+                    },
+                });
+            });
+        },
+    });
 
     return {
         Film,
