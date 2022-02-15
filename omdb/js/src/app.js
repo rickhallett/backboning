9803f5fd7c4105e479bb066a12652dd618daf031
Rick Hallett <rick.hallett@brandwatch.com>
2022-02-15 12:59:24 +0000

    model fetch with constructor id

--- omdb/js/src/app.js
+++ omdb/js/src/app.js
@@ -7,13 +7,4 @@ define([
     'src/views',
     'src/collections',
     'src/utilities',
-], function ($, _, Backbone, eventbus, models, views, collections, utils) {
-    console.log($);
-    console.log(_);
-    console.log(eventbus);
-    console.log(Backbone);
-    console.log(models.Film);
-    console.log(views.FilmView);
-    console.log(collections.Films);
-    console.log(utils);
-});
+], function ($, _, Backbone, eventbus, models, views, collections, utils) {});
--- omdb/js/src/models.js
+++ omdb/js/src/models.js
@@ -1,5 +1,14 @@
 define(['underscore', 'backbone'], function (_, Backbone) {
-    let Film = Backbone.Model.extend({});
+    let Film = Backbone.Model.extend({
+        initialize: function (options) {
+            this.url = `https://www.omdbapi.com/?i=${options.id}&apikey=6d7ddf73`;
+        },
+    });
+
+    const lotr = new Film({ id: 'tt0120737' });
+    lotr.fetch();
+
+    console.log(lotr);
 
     return {
         Film,
